943079d043ccadcf52d32205697cef2e856d78e7
Bertramoon <76817533+Bertramoon@users.noreply.github.com>
2022-10-23 23:04:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import os
 
+class A:
+    def __init__(self, uname, pwd):
+        self.uname = uname
+        self.pwd = pwd
+    
+    def print_all(self):
+        print(f"my username is {self.uname}, password is {self.pwd}")
 print(os.environ)
 print("-"*40)
 username = os.getenv("USERNAME")
@@ -7,3 +14,5 @@ print(username)
 password = os.getenv("PASSWORD")
 print(password)
 print("-"*40)
+a = A(username, password)
+a.print_all()
